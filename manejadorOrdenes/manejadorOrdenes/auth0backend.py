08b12a70729cc8b1b6f9a2c1651314b6317d0eaa
Felipe Bedoya <f.bedoya@uniandes.edu.co>
2021-11-10 18:48:23 -0500

    Update auth0backend.py

--- manejadorOrdenes/manejadorOrdenes/auth0backend.py
+++ manejadorOrdenes/manejadorOrdenes/auth0backend.py
@@ -3,28 +3,29 @@ from social_core.backends.oauth import BaseOAuth2
 
 
 class Auth0(BaseOAuth2):
+    """Auth0 OAuth authentication backend"""
     name = 'auth0'
     SCOPE_SEPARATOR = ' '
     ACCESS_TOKEN_METHOD = 'POST'
-    EXTRA_DATA = [
-        ('picture', 'picture')
-    ]
+    EXTRA_DATA = [('picture', 'picture')]
 
     def authorization_url(self):
+        """Return the authorization endpoint."""
         return "https://" + self.setting('DOMAIN') + "/authorize"
 
-    def access_toke_url(self):
+    def access_token_url(self):
+        """Return the token endpoint."""
         return "https://" + self.setting('DOMAIN') + "/oauth/token"
 
     def get_user_id(self, details, response):
-        return response['user_id']
+        """Return current user id."""
+        return details['user_id']
 
     def get_user_details(self, response):
-        url = "https://" + self.setting('DOMAIN') + "/userinfo"
+        url = 'https://' + self.setting('DOMAIN') + '/userinfo'
         headers = {'authorization': 'Bearer ' + response['access_token']}
         resp = requests.get(url, headers=headers)
         userinfo = resp.json()
-
         return {'username': userinfo['nickname'],
                 'first_name': userinfo['name'],
                 'picture': userinfo['picture'],
@@ -38,6 +39,5 @@ class Auth0(BaseOAuth2):
         headers = {'authorization': 'Bearer ' + accessToken}
         resp = requests.get(url, headers=headers)
         userinfo = resp.json()
-        role = userinfo('https://isis2503-trempo.us.auth0.com/role')
-        return role
-
+        role = userinfo['https://isis2503-trempo.us.auth0.com/role']
+        return (role)
