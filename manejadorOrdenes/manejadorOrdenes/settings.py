804c161cab8eba2645c2778278e6a561ba625079
Felipe Bedoya <f.bedoya@uniandes.edu.co>
2021-11-18 23:49:02 -0500

    Revert "int8"
    This reverts commit 9709939a9088edae170d9e10dff9c36941f530d4.

--- manejadorOrdenes/manejadorOrdenes/settings.py
+++ manejadorOrdenes/manejadorOrdenes/settings.py
@@ -117,7 +117,6 @@ AUTH_PASSWORD_VALIDATORS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
