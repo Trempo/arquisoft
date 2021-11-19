3aca0a35cff5275c211eab676292e9a4de06c0db
Felipe Bedoya <f.bedoya@uniandes.edu.co>
2021-11-18 23:48:57 -0500

    Revert "int 10"
    This reverts commit 75f79a467edaeee2e60fd62ff17225bfb26c7448.

--- manejadorOrdenes/manejadorOrdenes/settings.py
+++ manejadorOrdenes/manejadorOrdenes/settings.py
@@ -153,5 +153,13 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 #USE_X_FORWARDED_HOST = True
 #FORCE_SCRIPT_NAME = '/ordenes'
