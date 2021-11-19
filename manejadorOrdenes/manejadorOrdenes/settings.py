d9c5e14e0b4df6aa70f1e0facdd29457d207ccab
Diego Granada M <diegogranada7@gmail.com>
2021-11-18 23:05:40 -0500

    try1

--- manejadorOrdenes/manejadorOrdenes/settings.py
+++ manejadorOrdenes/manejadorOrdenes/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/ordenes/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
