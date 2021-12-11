07e5e3353fcb241f5a7be787507cbbf133e72741
coldblade2000 <diegogranada7@gmail.com>
2021-12-10 23:13:58 -0500

    Intento aun otra 2

--- manejadorOrdenes/manejadorOrdenes/settings.py
+++ manejadorOrdenes/manejadorOrdenes/settings.py
@@ -128,11 +128,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+FORCE_SCRIPT_NAME = '/orders'
 
 STATIC_URL = '/orders/static/'
 WHITENOISE_STATIC_PREFIX = '/static'
 
-FORCE_SCRIPT_NAME = '/orders'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
