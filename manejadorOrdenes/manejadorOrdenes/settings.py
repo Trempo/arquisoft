8763b439aa140be13dc62a15f3eaf665951c3dec
coldblade2000 <diegogranada7@gmail.com>
2021-12-10 23:12:51 -0500

    Intento aun otra

--- manejadorOrdenes/manejadorOrdenes/settings.py
+++ manejadorOrdenes/manejadorOrdenes/settings.py
@@ -129,7 +129,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/orders/orders/static/'
+STATIC_URL = '/orders/static/'
+WHITENOISE_STATIC_PREFIX = '/static'
 
 FORCE_SCRIPT_NAME = '/orders'
 
