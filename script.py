8b02cd5bbce2c720aebf7d323fb2089c33582483
Andres Peña Diaz <andres@Andress-MacBook-Pro.local>
2021-12-11 03:04:11 -0500

    Facilidad de mantenimiento

---
+++ script.py
@@ -0,0 +1,17 @@
+import threading
+import os
+
+def execute():
+    threading.Timer(5.0, execute).start()
+    bashCmd = '''
+                cd ..
+                cd cd /home/ec2-user/arquisoft
+                git remote update
+                var="$(git status -uno)"
+                if [[ $var =~ "branch is behind" ]]; then
+                    sudo systemctl restart manejador-comercios.service
+                else 
+                    echo Up-To-Date
+                fi'''
+    os.system(bashCmd)
+execute()
---
+++ script2.py
@@ -0,0 +1,17 @@
+import threading
+import os
+
+def execute():
+    threading.Timer(5.0, execute).start()
+    bashCmd = '''
+                cd ..
+                cd /Users/andres/Documents/Uniandes/infracomp/infracomp-caso1
+                git remote update
+                var="$(git status -uno)"
+                if [[ $var =~ "branch is behind" ]]; then
+                    sudo systemctl restart manejador-ordenes.service
+                else 
+                    echo Up-To-Date
+                fi'''
+    os.system(bashCmd)
+execute()
